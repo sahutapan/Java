f28dd5ce23851c877fd22294778080df6df30954
Tapan Sahu <77159596+sahutapan@users.noreply.github.com>
2022-06-02 13:13:55 +0530

    Delete Set4 directory

--- Set4/Practical3/Sports.java
+++
@@ -1,6 +0,0 @@
-package sport;
-public interface Sports
-{
-    int SportWt=6;
-    public int getter();
-}
--- Set4/Practical8/tempCodeRunnerFile.java
+++
@@ -1,9 +0,0 @@
-
-    // public static void update(File myFile)
-    // {
-    //     String name,enroll;
-    //     System.out.print("Enter enrollment no:");
-    //     enroll=scan.nextLine();
-    //     System.out.print("Enter name no:");
-    //     name=scan.nextLine();
-    // }
